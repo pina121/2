4041d954820a07f68e637bd7b8702f0d14958c31
pina121 <pinuroy098@gmail.com>
2025-03-28 21:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,27 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, render_template, request, send_file
 from rembg import remove
 from PIL import Image
-import io
-import os
+from io import BytesIO
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['RESULT_FOLDER'] = 'results'
 
-# Create folders if they don't exist
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs(app.config['RESULT_FOLDER'], exist_ok=True)
-
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file uploaded', 400
+        file = request.files['file']
+        if file.filename == '':
+            return 'No file selected', 400
+        if file:
+            input_image = Image.open(file.stream)
+            output_image = remove(input_image, post_process_mask=True)
+            img_io = BytesIO()
+            output_image.save(img_io, 'PNG')
+            img_io.seek(0)
+            # return send_file(img_io, mimetype='image/png')  # Change download in separatre browser tab
+            return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='_rmbg.png')
     return render_template('index.html')
 
-@app.route('/remove-bg', methods=['POST'])
-def remove_background():
-    if 'image' not in request.files:
-        return 'No image uploaded', 400
-    
-    file = request.files['image']
-    if file.filename == '':
-        return 'No image selected', 400
-    
-    # Read the image
-    input_image = Image.open(file.stream)
-    
-    # Remove the background
-    output_image = remove(input_image)
-    
-    # Save the result to a BytesIO object
-    img_byte_arr = io.BytesIO()
-    output_image.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-    
-    # Save the processed image
-    result_path = os.path.join(app.config['RESULT_FOLDER'], f"result_{file.filename.split('.')[0]}.png")
-    output_image.save(result_path)
-    
-    # Return the processed image
-    return send_file(img_byte_arr, mimetype='image/png', as_attachment=True, 
-                    download_name=f"bg_removed_{file.filename.split('.')[0]}.png")
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5100)
